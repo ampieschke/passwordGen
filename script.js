b5c3c4f08237032974e0a919fa2767331507fa5f
Andrew Pieschke <ampieschke@gmail.com>
2020-12-09 22:20:04 -0500

    created prompts

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  prompt ("How long of a password would you like? (8-128 characters)")
+  prompt ("Would you like lowercase letters? (abc...)")
+  prompt ("Would you like uppercase letters? (ABC...)")
+  prompt ("Would you like numeric characters? (123...)")
+  prompt ("Would you like special characters? (!`$...)")
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
