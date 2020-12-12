c4fb7877e0c7f4777dfe1e7c4bd413ed94066b60
Andrew Pieschke <ampieschke@gmail.com>
2020-12-12 15:53:12 -0500

    fixed character variables and began pseudo coding my work flow

--- script.js
+++ script.js
@@ -7,44 +7,41 @@ function writePassword() {
   // check for password length
  var total = prompt("How long of a password would you like? (8-128 characters)");
   console.log(total);
-
-  // chedk for lowercase letters
-  var wantLowercase = confirm("Click 'OK' if you would like lowercase letters? (abc...)");
-  if (wantLowercase == true) {
-    console.log("wants lowercase characters")
-  }
-
-  //check for uppercase letters
-  var wantUppercase = confirm("Click 'OK' if you would like uppercase letters? (ABC...)");
-  if (wantUppercase == true) {
-    console.log("wants uppercase characters")
-  }
-
-  //check for numeric characters
-  var wantNumeric = confirm("Click 'OK' if you would like numeric characters? (123...)");
-if (wantNumeric == true) {
-  console.log("wants numeric characters")
+  
+if (total < 8 || total >128) {
+alert("Must be between 8 and 128 characters!")
+var total = prompt("How long of a password would you like? (8-128 characters)");
+  console.log(total);
 }
 
-//check for special characters
+// variables
+  var wantLowercase = confirm("Click 'OK' if you would like lowercase letters? (abc...)");
+  var wantUppercase = confirm("Click 'OK' if you would like uppercase letters? (ABC...)");
+  var wantNumeric = confirm("Click 'OK' if you would like numeric characters? (123...)");
   var wantSpecial = confirm("Click 'OK' if you would like special characters? (!`$...)");
-  if (wantSpecial == true) {
-    console.log("wants special characters")
-  }
+
   
 // characters
-// var lowercase = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
-// var uppercase = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
-var numerals = [0,1,2,3,4,5,6,7,8,9];
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numerals = ["0","1","2","3","4","5","6","7","8","9"];
+var specials = ["~","`","!","@","#","$","%","^","&","*","(",")","_","+","-","=","{","}","[","]",":",";","<",">","?",",",".","/","|"];
 
 var randomNumerals = numerals[Math.floor(Math.random()*numerals.length)];
 console.log(randomNumerals)
 
-//var specials = [~,`,!,@,#,$,%,^,&,*,(,),_,+,-,=,{,},[,],:,",;,\,',<,>,?,,,.,/,|,\,\];
+var completePassword = (lowercase[Math.floor(Math.random() * lowercase.length)]);
+console.log(completePassword) + (uppercase[Math.floor(Math.random() * uppercase.length)]);
+console.log(completePassword) + (numerals[Math.floor(Math.random() * numerals.length)]);
+console.log(completePassword) + (specials[Math.floor(Math.random() * specials.length)]);
+console.log(completePassword)
+
+
 
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var completePassword = "";
 
 
   passwordText.value = password;
